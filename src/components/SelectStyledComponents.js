01bdf456c69b21ec71f6a838f13d9c2fe8550648
최훈오 <gnsdh86166@gmail.com>
2023-05-25 22:09:17 +0900

    feat: Select 페이지를 캐러셀로 구현

--- src/components/SelectStyledComponents.js
+++ src/components/SelectStyledComponents.js
@@ -4,26 +4,16 @@ export const Wrapper = styled.div`
   max-width: 600px;
   min-width: 360px;
   margin: 0 auto;
+  position: relative;
   width: 100%;
   height: 100%;
 `;
 
-export const SelectContainer = styled.div`
-  display: flex;
-  transition: ${({ fade }) => fade && "opacity 0.5s ease-in-out;"} 
-  background: ${({ id }) =>
-    `linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(../img/selectbackground${
-      id + 1
-    }.jpg)`};
-  flex-direction: column;
-  opacity:${({ fade }) => (fade ? "1" : "0")};
-  width: 100%;
-  height: 800px;
-`;
-
 export const Title = styled.div`
   padding: 0 37.5px;
-  margin-top: 120px;
+  position: absolute;
+  width: 100%;
+  top: 120px;
   span {
     justify-content: center;
     display: flex;
@@ -35,10 +25,12 @@ export const Title = styled.div`
 `;
 
 export const SelectBox = styled.div`
+  position: absolute;
+  top: 49%;
   display: flex;
+  flex-direction: column;
   justify-content: space-between;
   width: 100%;
-  margin: 203px auto 0 auto;
   font-weight: 300;
   font-size: 15px;
   line-height: 24px;
@@ -55,26 +47,26 @@ export const SelectBox = styled.div`
 `;
 
 export const BackButton = styled.button`
-  background: url(/img/back.png);
-  margin-left: 20px;
-  width: 48px;
   border: none;
   cursor: pointer;
+  left: 4%;
+  z-index: 3;
 `;
 export const NextButton = styled.button`
-  background: url(/img/next.png);
-  width: 48px;
   border: none;
   cursor: pointer;
-  margin-right: 20px;
+  right: 8%;
+  z-index: 3;
 `;
 
 export const GoButton = styled.button`
+  position: absolute;
+  bottom: 64px;
+  margin: 0 30px;
   background: #ffc300;
   border-radius: 27px;
   width: 89%;
   height: 54px;
-  margin: 234px auto 63px auto;
   font-size: 20px;
   font-weight: 600;
   border: none;
--- src/pages/Select.js
+++ src/pages/Select.js
@@ -1,76 +1,101 @@
 import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 import {
-  SelectBox,
-  SelectContainer,
   Title,
   Wrapper,
   NextButton,
   BackButton,
   GoButton,
+  SelectBox,
 } from "../components/SelectStyledComponents";
 import { region } from "../region";
 import { useNavigate, useParams } from "react-router-dom";
 
-function Select() {
-  const navigate = useNavigate();
-  const [fade, setFade] = useState(0);
-  let { id } = useParams();
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Image from "./Image";
 
-  useEffect(() => {
-    setTimeout(() => {
-      setFade("end");
-    }, 100);
-    return () => {
-      setFade("");
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [id]);
+const StyledSlider = styled(Slider)`
+  height: 100%;
+  width: 100%;
+  position: relative;
+  .slick-prev::before,
+  .slick-next::before {
+    opacity: 0;
+    display: none;
+  }
+`;
+
+export default function Select() {
+  const navigate = useNavigate();
+  const [id, setId] = useState(0);
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: (
+      <NextButton>
+        <img
+          src="/img/next.png"
+          alt=""
+          onClick={() => {
+            if (Number(id) === 5) {
+              setId(Number(id) - 5);
+            } else {
+              setId(Number(id) + 1);
+            }
+          }}
+        />
+      </NextButton>
+    ),
+    prevArrow: (
+      <BackButton>
+        <img
+          src="/img/back.png"
+          alt=""
+          onClick={() => {
+            if (Number(id) === 0) {
+              setId(Number(id) + 5);
+            } else {
+              setId(Number(id) - 1);
+            }
+          }}
+        />
+      </BackButton>
+    ),
+  };
 
   return (
     <Wrapper>
-      <SelectContainer id={Number(id)} fade={fade}>
-        <Title>
-          <span>
-            Choose the place
-            <br />
-            you want to visit
-          </span>
-        </Title>
-        <SelectBox>
-          <BackButton
-            onClick={() => {
-              if (Number(id) === 0) {
-                navigate(`/select/${Number(id) + 5}`);
-              } else {
-                navigate(`/select/${Number(id) - 1}`);
-              }
-            }}
-          />
-          <div>
-            <span className="city">{region[Number(id)].city}</span>
-            <span className="area">{region[Number(id)].area}</span>
-          </div>
-          <NextButton
-            onClick={() => {
-              if (Number(id) === 5) {
-                navigate(`/select/${Number(id) - 5}`);
-              } else {
-                navigate(`/select/${Number(id) + 1}`);
-              }
-            }}
-          />
-        </SelectBox>
-
-        <GoButton
-          onClick={() => {
-            navigate(`/map/${Number(id)}`);
-          }}
-        >
-          Go
-        </GoButton>
-      </SelectContainer>
+      <StyledSlider {...settings}>
+        {[0, 0, 0, 0, 0, 0].map((item) => {
+          return (
+            <div>
+              <Image img={`/img/selectbackground${Number(id) + 1}.jpg`} />
+            </div>
+          );
+        })}
+      </StyledSlider>
+      <Title>
+        <span>
+          Choose the place
+          <br />
+          you want to visit
+        </span>
+      </Title>
+      <SelectBox>
+        <span className="city">{region[Number(id)].city}</span>
+        <span className="area">{region[Number(id)].area}</span>
+      </SelectBox>
+      <GoButton
+        onClick={() => {
+          navigate(`/map/${Number(id)}`);
+        }}
+      >
+        Go
+      </GoButton>
     </Wrapper>
   );
 }
-
-export default Select;
