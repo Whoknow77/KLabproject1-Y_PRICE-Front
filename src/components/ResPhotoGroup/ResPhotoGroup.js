db52bccf16a48bc3973cde711e733b4ec82d20c2
최훈오 <gnsdh86166@gmail.com>
2023-07-19 21:47:19 +0900

    fix: 음식점 리뷰 사진이 없는 경우 오류 해결

--- src/components/ResPhotoGroup/ResPhotoGroup.js
+++ src/components/ResPhotoGroup/ResPhotoGroup.js
@@ -1,6 +1,10 @@
 import { PhotoGroup } from "./ResPhotoGroupStyledComponents";
 
 const ResPhotoGroup = ({ target }) => {
+  let isphoto = target[1].photo;
+  if (!isphoto) {
+    return <PhotoGroup></PhotoGroup>;
+  }
   return (
     <PhotoGroup>
       {Object.entries(target[1].photo).map(([key, value], index) => {
